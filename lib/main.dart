dd018af0e7058713347007011e35267848562cc9
zimvo <zimvo@busymed.co.za>
2022-02-05 11:32:49 +0200

    init push

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DiaryBook',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.green,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade100,
+        toolbarHeight: 100,
+        elevation: 4,
+        title: Row(
+          children: [
+            Text(
+              'my',
+              style: TextStyle(fontSize: 39, color: Colors.blueGrey.shade400),
+            ),
+            const Text(
+              'Diary',
+              style: TextStyle(fontSize: 39, color: Colors.green),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
